d7c8e5c9f61387771d28615ec3b50eac4b464983
Enzzo Ferrari Bertoncello <126869225+ebertoncello12@users.noreply.github.com>
2023-07-30 22:31:21 -0300

    Update rabbitmq.js

--- rabbitmq.js
+++ rabbitmq.js
@@ -1,12 +1,15 @@
 const amqp = require('amqplib');
 
+
+
 async function connectToRabbitMQ() {
   try {
     const connection = await amqp.connect('amqp://localhost'); // ou substitua por sua URL do RabbitMQ
     const channel = await connection.createChannel();
     const queue = 'car_created'; // Nome da fila para a mensagem do carro criado
 
-    await channel.assertQueue(queue, { durable: true });
+    await channel.assertQueue(queue, { durable: true }); // metodo para verificar se uma fila existe e a option para persisir ela no DB
+    // assim caso o servidor seja reiniciado nao ira perder a mensagem
     return { connection, channel, queue };
     
   } catch (error) {
