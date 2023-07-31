8473e01b564c1c2d5d0a4bf2de57416bac0065b3
Enzzo Ferrari Bertoncello <126869225+ebertoncello12@users.noreply.github.com>
2023-07-30 22:30:35 -0300

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); // CRM para interagir com DB de forma simples e rapida 
 
 const DB_URL = 'mongodb+srv://enzzo:enzzo@log.iusbitn.mongodb.net'; // Mude para a URL do seu banco de dados MongoDB
 const DB_NAME = 'log'; // Nome do banco de dados que conterá os logs
 
-async function connectToDatabase() {
+async function connectToDatabase() { // FUnçao assincrona para conexao com o banco de dados , 
+                                     //pois pode demorar um tempo para a conexao com o DB retornando uma promise 
+                                     // Usando o await o codigo sera pausado ate que a conexao seja estabelicida 
   try {
     await mongoose.connect(`${DB_URL}/${DB_NAME}`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      // Nenhum dos mecanismo sao OBRIGATORIOS so q para  garantir a melhor performance, compatibilidade e evitar problemas de atualizações futuras
+      useNewUrlParser: false, // Mecanisno para Analisar a URL , antigo esta desatualizado
+      useUnifiedTopology: true, // Mecanisno de Monitoramento
     });
     console.log('Conectado ao MongoDB com sucesso');
   } catch (error) {
@@ -16,4 +19,4 @@ async function connectToDatabase() {
   }
 }
 
-module.exports = connectToDatabase;
+module.exports = connectToDatabase; // Exportaçao para usar em qualquer arquivo do codigo mantendo o codigo CLEAN
