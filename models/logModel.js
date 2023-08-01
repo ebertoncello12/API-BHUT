c7bbbdfbe0ebba359086f89f039a798b6b4310db
Enzzo Ferrari Bertoncello <126869225+ebertoncello12@users.noreply.github.com>
2023-08-01 08:51:06 -0300

    Update logModel.js

--- models/logModel.js
+++ models/logModel.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const logSchema = new mongoose.Schema({
-  data_hora: { type: Date, default: Date.now, required: true },
-  car_id: { type: mongoose.Schema.Types.ObjectId, required: true },
+  data_hora: { type: Date, default: Date.now, required: true }, // Tipo data , valor default ou seja valor padrao sera sempre Data.now = Horario atual
+  car_id: { type: mongoose.Schema.Types.ObjectId, required: true }, // carid e o id do documento relacionado a o id do carro 
 });
 
-const Log = mongoose.model('log', logSchema);
+const Log = mongoose.model('log', logSchema); // Associoando o modelo logSchema a log do banco de dados
 
-module.exports = Log;
+module.exports = Log; // Exportaçao para usarmos em outra parte da aplicaçao 
